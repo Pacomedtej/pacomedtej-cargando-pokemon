288b8e924014acb0c882d4d58789f8397eb67233
Francisco Medina <pacomedtej@gmail.com>
2026-05-20 13:21:25 -0600

    Añadir Lucario, Pecharunt y Staraptor

--- data.js
+++ data.js
@@ -59,5 +59,53 @@ const EPISODES = [
       instagram: "https://www.instagram.com/reel/DXk9XOjDCKs/",
       facebook:  "https://www.facebook.com/share/v/18fd3pGWpv/?mibextid=wwXIfr"
     }
+  },
+  {
+    ep: 3,
+    id: 448,
+    name: "Lucario",
+    generation: 4,
+    types: ["lucha", "acero"],
+    weight_kg: 54,
+    cargado: true,
+    thumbnail: "img/thumbs/lucario.jpg",
+    links: {
+      youtube:   "https://youtube.com/shorts/eRQiYsJEA78?si=3hRBszAw_KTrSCIi",
+      tiktok:    "https://vt.tiktok.com/ZSxBtmM7p/",
+      instagram: "https://www.instagram.com/reel/DX5HSAjxnFQ/?igsh=YzBhOXBtcjRza3Vh",
+      facebook:  "https://www.facebook.com/share/v/14feBq6qHP9/?mibextid=wwXIfr"
+    }
+  },
+  {
+    ep: 4,
+    id: 1025,
+    name: "Pecharunt",
+    generation: 9,
+    types: ["veneno", "fantasma"],
+    weight_kg: 0.3,
+    cargado: true,
+    thumbnail: "img/thumbs/pecharunt.jpg",
+    links: {
+      youtube:   "https://youtube.com/shorts/_09yqwWsfFY?si=PlIaGRVIZtzBdZmR",
+      tiktok:    "https://vt.tiktok.com/ZSxBtfmdJ/",
+      instagram: "https://www.instagram.com/reel/DYIOySURuwF/?igsh=OHN6c3lkM29lZWZr",
+      facebook:  "https://www.facebook.com/share/v/1GYqoRCNE5/?mibextid=wwXIfr"
+    }
+  },
+  {
+    ep: 5,
+    id: 398,
+    name: "Staraptor",
+    generation: 4,
+    types: ["normal", "volador"],
+    weight_kg: 24.9,
+    cargado: true,
+    thumbnail: "img/thumbs/staraptor.jpg",
+    links: {
+      youtube:   "https://youtube.com/shorts/mihYNXBmUVs?si=5xab-39H5KRUB04F",
+      tiktok:    "https://vt.tiktok.com/ZSxBtDjWp/",
+      instagram: "https://www.instagram.com/reel/DYYO-iyRxBR/?igsh=MWV0dGMxd3Byc25ncg==",
+      facebook:  "https://www.facebook.com/share/v/1C7rVHwTHp/?mibextid=wwXIfr"
+    }
   }
 ];
